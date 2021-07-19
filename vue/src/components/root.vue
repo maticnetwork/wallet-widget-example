7ce48c6b82315581cec22d0a6554a17fd9641040
ujjwalgupta94 <ujjwal@matic.network>
2021-07-19 08:21:06 +0530

    ADD VUE EXAMPLE

---
+++ vue/src/components/root.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="col content-center mt-20px">
+    <h1 class="primary-text">Welcome to my awesome website</h1>
+    <button class="btn primary x-big mt-20px mb-10px ripple" id="btnOpenWidget">Open widget</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ vue/src/index.ts
@@ -0,0 +1,18 @@
+import { Widget } from "@maticnetwork/wallet-widget";
+import Vue from "vue";
+import Root from "./components/root.vue";
+import "flexstyle";
+
+const widget = new Widget({
+    target: '#btnOpenWidget',
+    appId: "test",
+    position:"bottom-right"
+});
+
+const vue = new Vue({
+    render: h => h(Root),
+    el: '#app',
+    mounted() {
+        widget.create()
+    }
+})
